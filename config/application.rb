6445350b79c65894a2c7d329cf98c65e723932a9
Evan <gthrowaway28@gmail.com>
2017-08-04 11:46:17 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,5 @@ module Sharkos
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config/application.rb
-config.assets.initialize_on_precompile = false
   end
 end
